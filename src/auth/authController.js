ba6f3a7e0555b3396fc8c9c1c4143c7ebfb32fbf
faqihss <faqihsetiawan17@gmail.com>
2024-06-02 12:57:00 +0700

    added configuration

--- src/auth/authController.js
+++ src/auth/authController.js
@@ -1,42 +1,43 @@
-    const Joi = require('joi');
-    const { register, login } = require('./authService');
+const Joi = require('joi');
+const { register, login } = require('./authService');
 
-    async function registerHandler(request, h) {
-        const schema = Joi.object({
-            email: Joi.string().email().required(),
-            password: Joi.string().min(6).required()
-        });
+async function registerHandler(request, h) {
+    const schema = Joi.object({
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).required()
+    });
 
-        const { error, value } = schema.validate(request.payload);
-        if (error) {
-            return h.response({ status: 'fail', message: error.details[0].message }).code(400);
-        }
-
-        try {
-            const user = await register(value);
-            return h.response({ status: 'success', user }).code(201);
-        } catch (err) {
-            return h.response({ status: 'fail', message: err.message }).code(400);
-        }
+    const { error, value } = schema.validate(request.payload);
+    if (error) {
+        return h.response({ status: 'fail', message: error.details[0].message }).code(400);
     }
 
-    async function loginHandler(request, h) {
-        const schema = Joi.object({
-            email: Joi.string().email().required(),
-            password: Joi.string().min(6).required()
-        });
+    try {
+        const user = await register(value);
+        return h.response({ status: 'success', user }).code(201);
+    } catch (err) {
+        return h.response({ status: 'fail', message: err.message }).code(400);
+    }
+}
 
-        const { error, value } = schema.validate(request.payload);
-        if (error) {
-            return h.response({ status: 'fail', message: error.details[0].message }).code(400);
-        }
+async function loginHandler(request, h) {
+    const schema = Joi.object({
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).required()
+    });
 
-        try {
-            const token = await login(value);
-            return h.response({ status: 'success', token }).code(200);
-        } catch (err) {
-            return h.response({ status: 'fail', message: err.message }).code(400);
-        }
+    const { error, value } = schema.validate(request.payload);
+    if (error) {
+        return h.response({ status: 'fail', message: error.details[0].message }).code(400);
     }
 
-    module.exports = { registerHandler, loginHandler };
+    try {
+        const token = await login(value);
+        return h.response({ status: 'success', token }).code(200);
+    } catch (err) {
+        return h.response({ status: 'fail', message: err.message }).code(400);
+    }
+}
+
+
+module.exports = { registerHandler, loginHandler };
